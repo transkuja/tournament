7e2ea3297912c3ca2cafa7dd1269d0140fddba95
Tayho <theau.bonnet@gmail.com>
2023-03-13 21:20:27 +0100

    Rajout de la règle spécial mika

--- Assets/Scripts/Rounds/Details/OCG2RoundDetails.cs
+++ Assets/Scripts/Rounds/Details/OCG2RoundDetails.cs
@@ -63,14 +63,39 @@ public class OCG2RoundDetails
     public void SetTeamsInSlot(List<int> teamIds)
     {
         List<Match> UsedMatch = new List<Match>();
+        List<KeyValuePair<int,int>> TeamPairs = new List<KeyValuePair<int, int>>();
         //Priorisation de la salle ciné
         for(int i = 0;i < teamIds.Count-1;i+=2)
         {
-            Match matchToUse;
+            TeamPairs.Add(new KeyValuePair<int, int>(teamIds[i],teamIds[i+1]));
+        }
+        
+        //Cas mika
+        KeyValuePair<int, int> MikaPair = new KeyValuePair<int, int>(-1,-1);
+        foreach (var teamPair in TeamPairs)
+        {
+            if(teamPair.Key == 7 || teamPair.Value == 7)
+            {
+                MikaPair = teamPair;
+                Match matchToUse = MatchUtils.FindMatchInRoom(UsedMatch,Room.CinemaRoom);
+                UsedMatch.Add(matchToUse);
+                SetTeamInSlot(matchToUse,TeamSlot.Team1,MikaPair.Key);
+                SetTeamInSlot(matchToUse,TeamSlot.Team2,MikaPair.Value);
+                break;
+            }
+        }
+        
+        TeamPairs.Remove(MikaPair);
 
-            int Team1Count = OCG2DetailsDatabase.Instance.GetTeamPlayedInRoomCount(teamIds[i], Room.CinemaRoom);
-            int Team2Count = OCG2DetailsDatabase.Instance.GetTeamPlayedInRoomCount(teamIds[i + 1], Room.CinemaRoom);
-            Debug.Log($"Team {teamIds[i]} : {Team1Count} | Team {teamIds[i + 1]} : {Team2Count}");
+        foreach (var teamPair in TeamPairs)
+        {
+            Match matchToUse;
+            int team1Id = teamPair.Key;
+            int team2id = teamPair.Value;
+            
+            int Team1Count = OCG2DetailsDatabase.Instance.GetTeamPlayedInRoomCount(team1Id, Room.CinemaRoom);
+            int Team2Count = OCG2DetailsDatabase.Instance.GetTeamPlayedInRoomCount(team2id, Room.CinemaRoom);
+            Debug.Log($"Team {team1Id} : {Team1Count} | Team {team2id} : {Team2Count}");
             //if TeamPair has not match in cinema room search for a match in cinema room that can be used
             if (Team1Count <= 0 ||
                 Team2Count <= 0)
@@ -83,9 +108,11 @@ public class OCG2RoundDetails
             }
             
             UsedMatch.Add(matchToUse);
-            SetTeamInSlot(matchToUse,TeamSlot.Team1,teamIds[i]);
-            SetTeamInSlot(matchToUse,TeamSlot.Team2,teamIds[i+1]);
+            SetTeamInSlot(matchToUse,TeamSlot.Team1,team1Id);
+            SetTeamInSlot(matchToUse,TeamSlot.Team2,team2id);
         }
+        
+
 
         
        /* for (int i = 0; i < teamIds.Count; i++)
